daf871d479962eb8071c156c70de4d2340b2b157
Simon Burfield <simon.burfield@o2.com>
2015-08-07 09:36:34 +0100

    Tidy up

--- BurfWorld/core/src/com/burfdevelopment/burfworld/Screens/GameRenderScreen.java
+++ BurfWorld/core/src/com/burfdevelopment/burfworld/Screens/GameRenderScreen.java
@@ -5,14 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g3d.Material;
-import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.math.collision.BoundingBox;
@@ -43,9 +38,6 @@ public class GameRenderScreen  implements Screen {
     public static int height() { return Gdx.graphics.getHeight(); }
 
     private ModelBatch modelBatch;
-    private Model box;
-    private Model floor;
-
     private Array<GameObject> boxInstance;
     private Array<Chunk> chunks;
     private Vector3 oldPosition = new Vector3();
@@ -102,41 +94,12 @@ public class GameRenderScreen  implements Screen {
 
     }
 
-    @Override
-    public void render(float delta) {
+    public void checkCollison()
+    {
 
-        update();
+        //        float cubeSize = 1.2f;
 
-        Gdx.gl20.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-        //TODO do we need all of these?
-        //Do all your basic OpenGL ES setup to start the screen render.
-        Gdx.gl20.glClearColor(0.0f, 0.3f, 0.5f, 1);
-        Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-
-        // Like spriteBatch, just with models!  pass in the box Instance and the environment
-        //Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
-        modelBatch.begin(camera);
-
-        Skybox.update(camera.position);
-        modelBatch.render(Skybox.modelInstance);
-
-//        float cubeSize = 1.2f;
-
-        accum += Gdx.graphics.getDeltaTime();
-
-        if(accum >= TICK)
-        {
-            //Gdx.app.log("MyTag 2", "X " + (int)camera.position.x / 16 + " z " + (int)camera.position.z / 16);
-            //Gdx.app.log("MyTag 2", "x" + camera.direction.toString());
-            Gdx.app.log("MyTag 2", "count" + chunks.size);
-            accum = 0.0f;
-
-            executor.submit(task);
-
-        }
-
-//        for (int i = 0; i < boxInstance.size; i ++ ) {
+        //        for (int i = 0; i < boxInstance.size; i ++ ) {
 //            modelBatch.render(boxInstance.get(i));
 //
 //
@@ -169,7 +132,35 @@ public class GameRenderScreen  implements Screen {
 
 //        }
 
+    }
 
+    @Override
+    public void render(float delta) {
+
+        update(); // controls
+
+        Gdx.gl20.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        //TODO do we need all of these?
+        //Do all your basic OpenGL ES setup to start the screen render.
+        Gdx.gl20.glClearColor(0.0f, 0.3f, 0.5f, 1);
+        Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        // Like spriteBatch, just with models!  pass in the box Instance and the environment
+        //Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
+        modelBatch.begin(camera);
+
+        Skybox.update(camera.position);
+        modelBatch.render(Skybox.modelInstance);
+        accum += Gdx.graphics.getDeltaTime();
+
+        if(accum >= TICK) // fire off chunk builder
+        {
+            Gdx.app.log("MyTag 2", "count" + chunks.size);
+            accum = 0.0f;
+            executor.submit(task);
+
+        }
 
         for (int i = 0; i < chunks.size; i ++ ) {
                 chunks.get(i).render(modelBatch, camera);
@@ -215,8 +206,7 @@ public class GameRenderScreen  implements Screen {
         stage.dispose();
         font.dispose();
         modelBatch.dispose();
-        box.dispose();
-        floor.dispose();
+
         Skybox.disable();
 
     }
@@ -228,40 +218,6 @@ public class GameRenderScreen  implements Screen {
         font.draw(stage.getBatch(), "Mem: " + Gdx.app.getJavaHeap() / 1000000f + " " + Gdx.app.getNativeHeap() / 1000000f,  10, Gdx.graphics.getHeight() - 30);
         stage.getBatch().end();
     }
-
-//    public void hitSomething(int screenX, int screenY) {
-//        Ray pickRay = camera.getPickRay(screenX, screenY);
-//
-//        // A bounding box for each of your minecraft blocks
-//        BoundingBox boundingBox = new BoundingBox();
-//        Vector3 intersection = new Vector3();
-//        if (Intersector.intersectRayBounds(pickRay, boundingBox, intersection)) {
-//            // The ray has hit the box, intersection is the point it hit
-//            Gdx.app.log("MyTag", "my informative message");
-//        } else {
-//            // Not hit
-//            Gdx.app.log("MyTag", "my informative message2");
-//        }
-//    }
-
-//    public void hitSomething(int screenX, int screenY) {
-//        // If you are only using a camera
-//        Ray pickRay = camera.getPickRay(screenX, screenY);
-////        // If your camera is managed by a viewport
-//        //Ray pickRay = stage.getViewport().getPickRay(screenX, screenY);
-//
-//        // we want to check a collision only on a certain plane, in this case the X/Z plane
-//        Plane plane = new Plane(new Vector3(0, -1, 0), -1);
-//        Vector3 intersection = new Vector3();
-//
-//        Intersector.intersectRayPlane(pickRay, plane, intersection);
-//
-//        int x = (int)intersection.x;
-//        int z = (int)intersection.z;
-//
-//        Gdx.app.log("MyTag", "x " + x  + " z "+ z);
-//
-//    }
 
     public int getObject (int screenX, int screenY) {
 
@@ -318,56 +274,6 @@ public class GameRenderScreen  implements Screen {
         return 1;
     };
 
-
-    public void cubes()
-    {
-        // A ModelBatch is like a SpriteBatch, just for models.  Use it to batch up geometry for OpenGL
-        modelBatch = new ModelBatch();
-
-        // A ModelBuilder can be used to build meshes by hand
-        ModelBuilder modelBuilder = new ModelBuilder();
-
-        // It also has the handy ability to make certain premade shapes, like a Cube
-        // We pass in a ColorAttribute, making our cubes diffuse ( aka, color ) red.
-        // And let openGL know we are interested in the Position and Normal channels
-        box = modelBuilder.createBox(1f, 1f, 1f,
-                new Material(ColorAttribute.createDiffuse(Color.BLUE)),
-                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates
-        );
-
-        // A model holds all of the information about an, um, model, such as vertex data and texture info
-        // However, you need an instance to actually render it.  The instance contains all the
-        // positioning information ( and more ).  Remember Model==heavy ModelInstance==Light
-
-        int c = 40;
-        for (int x = 0; x < c; x ++ ) {
-
-            for (int y = 0; y < c; y ++ ) {
-
-                for (int z = 0; z < c; z ++ ) {
-
-                    GameObject m = new GameObject(box,new Vector3(x + 2,y,z), true);
-                    boxInstance.add(m);
-                }
-
-            }
-
-        }
-
-        Material matWhite = new Material(ColorAttribute.createDiffuse(Color.WHITE));
-
-        float size = 10;
-        modelBuilder.begin();
-        MeshPartBuilder tileBuilder;
-        tileBuilder = modelBuilder.part("top", GL20.GL_TRIANGLES, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates, matWhite);
-        tileBuilder.rect(-size, 0.1f, size,   size, 0.1f, size,    size, 0.1f, -size,  -size, 0.1f, -size,  0f, 1f, 0f);
-        floor = modelBuilder.end();
-
-        GameObject m = new GameObject(floor, new Vector3(0,-1,0), false);
-        boxInstance.add(m);
-    }
-
-
     public void markAddChunk(float x, float z)
     {
         boolean found = false;
@@ -402,111 +308,6 @@ public class GameRenderScreen  implements Screen {
             }
         }
 
-
-
-//        if (direction.z < 0)
-//        {
-//            //Gdx.app.log("MyTag 2", "Fire A");
-//            if (direction.x < -0.5)
-//            {
-//                //Gdx.app.log("MyTag 2", "Fire 1");
-//
-////                markAddChunk(x - 2, z);
-////                markAddChunk(x - 2, z - 1);
-////                markAddChunk(x - 2, z + 1);
-//
-//                markAddChunk(x - 1, z);
-//                markAddChunk(x - 1, z - 1);
-//                markAddChunk(x - 1, z + 1);
-//
-//                markAddChunk(x, z - 1);
-//                markAddChunk(x , z + 1);
-//            }
-//            else if  (direction.x > 0.5)
-//            {
-//
-//                //Gdx.app.log("MyTag 2", "Fire 2");
-//
-////                markAddChunk(x + 2, z);
-////                markAddChunk(x + 2, z - 1);
-////                markAddChunk(x + 2, z + 1);
-//
-//                markAddChunk(x + 1, z);
-//                markAddChunk(x + 1, z - 1);
-//                markAddChunk(x + 1, z + 1);
-//
-//                markAddChunk(x, z - 1);
-//                markAddChunk(x , z + 1);
-//
-//            }
-//            else
-//            {
-//
-//                //Gdx.app.log("MyTag 2", "Fire 3");
-////                markAddChunk(x, z - 2);
-////                markAddChunk(x - 1, z - 2);
-////                markAddChunk(x + 1, z - 2);
-//
-//                markAddChunk(x, z - 1);
-//                markAddChunk(x - 1, z - 1);
-//                markAddChunk(x + 1, z - 1);
-//
-//                markAddChunk(x - 1, z );
-//                markAddChunk(x + 1, z );
-//            }
-//
-//        }
-//        else
-//        {
-//            //Gdx.app.log("MyTag 2", "Fire B");
-//
-//            if (direction.x < -0.5)
-//            {
-//
-//                //Gdx.app.log("MyTag 2", "Fire 2");
-////                markAddChunk(x - 2, z);
-////                markAddChunk(x - 2, z - 1);
-////                markAddChunk(x - 2, z + 1);
-//
-//                markAddChunk(x - 1, z);
-//                markAddChunk(x - 1, z - 1);
-//                markAddChunk(x - 1, z + 1);
-//
-//                markAddChunk(x, z - 1);
-//                markAddChunk(x , z + 1);
-//            }
-//            else if (direction.x > 0.5)
-//            {
-//
-//                //Gdx.app.log("MyTag 2", "Fire 3");
-////                markAddChunk(x + 2,z);
-////                markAddChunk(x + 2, z - 1);
-////                markAddChunk(x + 2, z + 1);
-//
-//                markAddChunk(x + 1,z);
-//                markAddChunk(x + 1, z - 1);
-//                markAddChunk(x + 1, z + 1);
-//
-//                markAddChunk(x, z - 1);
-//                markAddChunk(x , z + 1);
-//            }
-//            else
-//            {
-//
-//                //Gdx.app.log("MyTag 2", "Fire 4");
-////                markAddChunk(x,z + 2);
-////                markAddChunk(x - 1, z + 2);
-////                markAddChunk(x + 1, z + 2);
-//
-//                markAddChunk(x,z + 1);
-//                markAddChunk(x - 1, z + 1);
-//                markAddChunk(x + 1, z + 1);
-//
-//                markAddChunk(x - 1, z );
-//                markAddChunk(x + 1, z );
-//            }
-//        }
-
         for (int i = 0; i < chunks.size; i ++ ) {
 
             if (chunks.get(i).needed == false)
@@ -516,16 +317,7 @@ public class GameRenderScreen  implements Screen {
             }
 
         }
-
-        //chunks.add(new Chunk(new Vector3(0,0,-16)));
-        //chunks.add(new Chunk(new Vector3(16,0,0)));
-        //chunks.add(new Chunk(new Vector3(-16,0,0)));
-
-
-
-
     }
 
 }
-
 
