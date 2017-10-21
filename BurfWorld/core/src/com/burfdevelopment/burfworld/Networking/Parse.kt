f3adc41db29760c0423a926fa5546533412cfc3c
Simon Burfield <burf2000@gmail.com>
2017-10-21 20:57:42 +0100

    Updated networking

---
+++ BurfWorld/core/src/com/burfdevelopment/burfworld/Networking/Parse.kt
@@ -0,0 +1,48 @@
+package com.burfdevelopment.burfworld.Networking
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Net
+
+/**
+ * Created by burfies1 on 21/10/2017.
+ */
+
+class Parse : Net.HttpResponseListener {
+
+    private val app_id = "Dy95q3VpJ3z5Ppbl7dZhj4xmqY8vrj4Sck5VZWbA"
+    private val app_key = "1EleDLFzhQRRGChnoEhQ39QMWk03yB7O6Qu9lsZv"
+
+    fun add_net_score() {
+        // LibGDX NET CLASS
+        val httpPost = Net.HttpRequest(Net.HttpMethods.POST)
+        httpPost.url = "https://api.parse.com/1/classes/score/"
+        httpPost.setHeader("Content-Type", "application/json")
+        httpPost.setHeader("X-Parse-Application-Id", app_id)
+        httpPost.setHeader("X-Parse-REST-API-Key", app_key)
+        httpPost.content = "{\"score\": 1337, \"user\": \"CarelessLabs Java\"}"
+        Gdx.net.sendHttpRequest(httpPost, this@Parse)
+    }
+
+    fun get_net_score() {
+        // LibGDX NET CLASS
+        val httpGet = Net.HttpRequest(Net.HttpMethods.GET)
+        httpGet.url = "https://api.parse.com/1/classes/score/"
+        httpGet.setHeader("Content-Type", "application/json")
+        httpGet.setHeader("X-Parse-Application-Id", app_id)
+        httpGet.setHeader("X-Parse-REST-API-Key", app_key)
+        Gdx.net.sendHttpRequest(httpGet, this@Parse)
+    }
+
+    override fun handleHttpResponse(httpResponse: Net.HttpResponse) {
+        val statusCode = httpResponse.status.statusCode
+        println(statusCode.toString() + " " + httpResponse.resultAsString)
+    }
+
+    override fun failed(t: Throwable) {
+        println(t.message)
+    }
+
+    override fun cancelled() {
+
+    }
+}// may not need
