242dd87e5d0da4bec71ab96296f2d341bf553818
Simon Burfield <burf2000@gmail.com>
2017-10-31 19:23:48 +0000

    Full screen

--- BurfWorld/desktop/src/com/burfdevelopment/burfworld/desktop/DesktopLauncher.java
+++ BurfWorld/desktop/src/com/burfdevelopment/burfworld/desktop/DesktopLauncher.java
@@ -7,9 +7,9 @@ import com.burfdevelopment.burfworld.Activity.GameActivity;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
-		config.height = 600;
-
+		config.width = 1024;
+		config.height = 768;
+		config.fullscreen = true;
 		new LwjglApplication(new GameActivity(), config);
 	}
 }
